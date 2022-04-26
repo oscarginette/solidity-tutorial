4112a35250cc873de113e2d0f9187cd8788700a3
Oscar Ginette <oscar.ginette@dynatrace.com>
2022-04-26 16:49:52 +0200

    Create first keyboard contract

---
+++ contracts/Keyboards.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract Keyboards {
+    // You can kinda think of it as the easiest database you’ve ever worked with!
+    string[] public createdKeyboards;
+
+    function getKeyboards() view public returns (string[] memory) {
+        return createdKeyboards;
+    }
+}
