160d32075c5652c591b7d948b775e051b21479ad
Oscar Ginette <oscar.ginette@dynatrace.com>
2022-04-26 17:04:34 +0200

    Upate script to be run by multiple users

--- scripts/start.js
+++ scripts/start.js
@@ -1,20 +1,31 @@
 /**
- * An important note here: that hre.ethers is auto-imported when we run it using hardhat. 
+ * An important note here: that hre.ethers is auto-imported when we run it using hardhat.
  * You don’t need to import anything!
  */
 
 async function main() {
+  const [owner, somebodyElse] = await hre.ethers.getSigners();
   const keyboardsContractFactory = await hre.ethers.getContractFactory(
     "Keyboards"
   );
   const keyboardsContract = await keyboardsContractFactory.deploy();
   await keyboardsContract.deployed();
 
-  console.log("Contract deployed to:", keyboardsContract.address);
+  const keyboardTxn1 = await keyboardsContract.create(
+    "A really great keyboard!"
+  );
+  await keyboardTxn1.wait();
 
-  const keyboards = await keyboardsContract.getKeyboards();
+  const keyboardTx2 = await keyboardsContract
+    .connect(somebodyElse)
+    .create("An even better keyboard!");
+  await keyboardTx2.wait();
 
-  console.log("We got the Keyboards:", keyboards);
+  keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
+
+  keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+  console.log("And as somebody else!", keyboards);
 }
 
 main()
@@ -24,10 +35,10 @@ main()
     process.exit(1);
   });
 
-  // Output
-  /**
-   * npx hardhat run scripts/start.js
-   *   Compiled 1 Solidity file successfully
-   * Contract deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-   * We got the Keyboards: []
-   */
+// Output
+/**
+ * npx hardhat run scripts/start.js
+ *   Compiled 1 Solidity file successfully
+ * Contract deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+ * We got the Keyboards: []
+ */
