9f304ba784c16257c367537389e01c88bb50010d
Oscar Ginette <oscar.ginette@dynatrace.com>
2022-04-26 16:53:38 +0200

    Finish first script to deploy contract

---
+++ scripts/start.js
@@ -0,0 +1,28 @@
+async function main() {
+  const keyboardsContractFactory = await hre.ethers.getContractFactory(
+    "Keyboards"
+  );
+  const keyboardsContract = await keyboardsContractFactory.deploy();
+  await keyboardsContract.deployed();
+
+  console.log("Contract deployed to:", keyboardsContract.address);
+
+  const keyboards = await keyboardsContract.getKeyboards();
+
+  console.log("We got the Keyboards:", keyboards);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  // Output
+  /**
+   * npx hardhat run scripts/start.js                           1 ↵
+Compiled 1 Solidity file successfully
+Contract deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+We got the Keyboards: []
+   */
